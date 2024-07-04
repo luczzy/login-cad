c7be33b29a790533720637b1e2b93c7d600b06d8
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-07-04 09:03:59 -0300

    Atualiza arquivo js, adicionando function

---
+++ controller.js
@@ -0,0 +1,6 @@
+// VALIDAR ACESSO EM TELA DE LOGIN
+
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+}
