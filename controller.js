170f0437bf561df3d42d4d6483445c8e2cc704fb
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-08-02 11:52:03 -0300

    Adiciona o 'innerHTML', ele recebe o conteúdo da tabela e coloca no navegador

--- controller.js
+++ controller.js
@@ -14,17 +14,24 @@ function acessar(){
 
 
 // FUNÇÃO QUE ARMAZENA EM ARRAY NOME NA TELA DE CADASTRO
-
 var dadosLista = [];
-
 function salvarUser(){
    let nomeUser = document.getElementById('nomeUser').value;
 
    if(nomeUser){
     dadosLista.push(nomeUser);
-    console.log(dadosLista);
+    //console.log(dadosLista);
     document.getElementById('nomeUser').value = "";
    }else{
        alert("Favor informar o nome para cadastro");
    }
 }
+
+// FUNÇÃO PARA CRIAR LISTA
+function criaLista(){
+    let table = document.getElementById('table').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    for( let i = 0; i <= (dadosLista.length-1); i++){
+        tabela +="<tr><td>" + dadosLista[i] + "</td><td></td></tr>"; 
+        document.getElementById('table').innerHTML = table;
+        }
+    }
